73091e3fa2d3e1d76f91fef45d4762dae5c478f3
zhuchaobin <nanjing2007@163.com>
2018-12-25 23:32:56 +0800

    zhu

--- business-client/src/main/java/com/xai/tt/business/client/enums/UserType.java
+++ business-client/src/main/java/com/xai/tt/business/client/enums/UserType.java
@@ -3,11 +3,26 @@ package com.xai.tt.business.client.enums;
 public enum UserType {
 	/** 集团用户 */
 	Group, 
+	/** 平台 */
+	pltfrm, 
+	/** 上游供应商 */
+	ustrmSplr, 
+	/** 供应链公司 */
+	splchainCo, 
+	/** 融资企业 */
+	fncEntp, 
+	/** 保险公司 */
+	insCo, 
+	/** 银行 */
+	bnk, 
+	/** 物流公司 */
+	lgstcCo, 
+	/** 仓储公司 */
+	stgco, 
 	/** 厂商用户 */
 	Factory, 
 	/** 经销商用户 */
 	Dealer, 
 	/** 大客户用户 */
-	Company
-	
+	Company		
 }
