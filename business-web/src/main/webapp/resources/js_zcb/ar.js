2e6913126467593e21c637869240c131d740953c
zhuchaobin <nanjing2007@163.com>
2019-01-20 11:27:19 +0800

    zhu

--- business-web/src/main/webapp/resources/js_zcb/ar.js
+++ business-web/src/main/webapp/resources/js_zcb/ar.js
@@ -167,6 +167,48 @@
 				return value;
 		}
 		
+		 // 根据附件后缀返回文件类型
+		function getFileType(fileNm){ 
+			var index1=fileNm.lastIndexOf(".");
+			var index2=fileNm.length;
+			var suffix=fileNm.substring(index1+1,index2);//后缀名			
+			 var fileType = suffix;
+			if("jpg" == suffix || "png" == suffix || "bmp" == suffix || "gif" == suffix || "tif" == suffix || "pic" == suffix){
+				fileType = "[ 图片 ]";
+			} else if("docx" == suffix || "doc" == suffix){
+				fileType = "[ Word文档 ]";
+			} else if("xls" == suffix || "exls" == suffix){
+				fileType = "[ Excel文档 ]";
+			} else if("rar" == suffix || "zip" == suffix || "gz" == suffix){
+				fileType = "[ 压缩文档 ]";
+			} 
+			return fileType;
+		 }
+		// 引入日期时间格式转换
+		Date.prototype.Format = function(fmt)   
+		 { 
+		//author:wangweizhen
+		   var o = {   
+		     "M+" : this.getMonth()+1,                 //月份   
+		     "d+" : this.getDate(),                    //日
+		    "h+" : this.getHours(),                   //小时   
+		     "m+" : this.getMinutes(),                 //分   
+		     "s+" : this.getSeconds(),                 //秒   
+		     "q+" : Math.floor((this.getMonth()+3)/3), //季度   
+		     "S"  : this.getMilliseconds()             //毫秒   
+		   };   
+		   if(/(y+)/.test(fmt))   
+		     fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
+		   for(var k in o)   
+		     if(new RegExp("("+ k +")").test(fmt))   
+		   fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+		   return fmt;   
+		 }; 
+		 // js 一串数字1403149534转换为日期格式 
+		function transDateTime(timestamp){
+			var date = new Date(timestamp);    //根据时间戳生成的时间对象
+	        return date.format("yyyy-MM-dd hh:mm:ss");
+        }
 		function getArSubmmitDetail(id, arId, aplyPcstpCd){
  			// 带过来的环节编号
 			E7.post(pageSetting.resetPath + '/getArSubmmitDetail', {
@@ -190,9 +232,14 @@
 				var td = detail["t2UploadAtch01List"];
 				var insertHtml="";
 				$('#query_ar_atch_div2').hide();	
-				if(null != td && undifined != td){
+				if(null != td && undefined != td && ""!=td){
 					for ( var kk = 0; kk < td.length; kk++) {
-						insertHtml = insertHtml + '<div class="col-sm-2"><i class="iconfont icon-kl-fujian"></i><a href="'+ td[kk].srFileRte +'" target="_blank"><div>'+td[kk].oriFileNm+'<p>'+getFileType(td[kk].oriFileNm) + transDateTime(suffix) +'</div></a></div>';
+						var oriFileNm = td[kk].oriFileNm;
+						var index1=oriFileNm.lastIndexOf(".");
+						var index2=oriFileNm.length;
+						var suffix=oriFileNm.substring(index1+1,index2);//后缀名
+						//insertHtml = insertHtml + '<div class="col-sm-2"><i class="iconfont icon-kl-fujian"></i><a href="'+ td[kk].srFileRte +'" target="_blank"><div>'+td[kk].oriFileNm+'<p>'+getFileType(td[kk].oriFileNm) + transDateTime(td[kk].tms) +'</div></a></div>';
+						insertHtml = insertHtml + '<div class="col-sm-2"><i class="iconfont icon-kl-fujian"></i><a href="'+ td[kk].srFileRte +'" target="_blank"><div>'+td[kk].oriFileNm+'<p>'+getFileType(td[kk].oriFileNm)+'</div></a></div>';
 					}								
 					if(td.length > 0){
 						var testdiv = document.getElementById("upload_atch_div2");
